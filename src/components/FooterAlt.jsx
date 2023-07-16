ad985923ee2470fea802a6a4c1b9cac6824f8489
seanbuckle <seanlbuckle@gmail.com>
2023-07-17 00:58:51 +0100

    update 1 file and create 58 files

---
+++ src/components/FooterAlt.jsx
@@ -0,0 +1,17 @@
+export default function FooterAlt() {
+  return (
+    <footer>
+        Our company
+  Locations
+  Contact
+
+  Designo Central Office
+  3886 Wellington Street
+  Toronto, Ontario M9C 3J5
+
+  Contact Us (Central Office)
+  P : +1 253-863-8967
+  M : contact@designo.co
+    </footer>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function header() {
+  return (
+    <header>
+        <a>Designo</a>
+        <nav>
+            <a>Our Company</a>
+            <a>Locations</a>
+            <a>Contact</a>
+        </nav>
+        </header>
+  )
+}
